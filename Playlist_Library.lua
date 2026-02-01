9667787f44f09183ad0c2e9603948f8437602f54
senseikivotos2001-dotcom <sensei.kivotos2001@gmail.com>
2026-02-01 21:05:20 +0700

    Create Playlist_Library.lua

---
+++ Playlist_Library.lua
@@ -0,0 +1,13 @@
+--[[ 
+    FILE: Playlist_Library.lua
+    NHIỆM VỤ: LƯU TRỮ CÁC BẢN PIANO COVER ĐỈNH CAO
+]]
+
+local Playlist = {
+    ["GoldenHour"] = "rbxassetid://ID_1", -- Thay ID hoặc Link MP3 vào đây
+    ["aoharu"] = "rbxassetid://ID_2",
+    ["memory of kindness"] = "rbxassetid://ID_3",
+    ["nơi này có anh"] = "rbxassetid://ID_4"
+}
+
+return Playlist
